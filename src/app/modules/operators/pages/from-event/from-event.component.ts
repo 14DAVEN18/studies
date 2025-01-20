550c3899f2cbf5425501d87c9626185d43f433b0
David <dcortes79@uan.edu.co>
2025-01-20 17:57:54 -0500

    Implementación del componente para el operador fromEvent

--- src/app/modules/operators/pages/from-event/from-event.component.ts
+++ src/app/modules/operators/pages/from-event/from-event.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-from-event',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './from-event.component.html',
   styleUrl: './from-event.component.sass'
 })
 export class FromEventComponent {
+  public result: string = ''
+  private clickSubscription!: Subscription
 
+  public scrollResult: string = ''
+  private scrollSubscription!: Subscription
+  items = Array.from({length: 50}, (_, i) => i + 1)
+  
+
+  ngOnInit(): void {
+    const clickObservable = fromEvent<MouseEvent>(document, 'click')
+
+    this.clickSubscription = clickObservable.subscribe((event: MouseEvent) => {
+      this.result = `Hizo clic en: x=${event.clientX}, y=${event.clientY}`
+    })
+
+    const scrollContainer = document.querySelector('.scroll-container') as HTMLElement
+
+    if(scrollContainer) {
+      this.scrollSubscription = fromEvent(scrollContainer, 'scroll', { passive: true }).subscribe(() => {
+        const scrollTop = scrollContainer.scrollTop
+        const scrollHeight = scrollContainer.scrollHeight - scrollContainer.clientHeight
+        this.scrollResult = `posición de scroll: ${scrollTop} / ${scrollHeight}`
+      })
+    }
+  }
+
+  ngOnDestroy() {
+    if(this.clickSubscription)
+      this.clickSubscription.unsubscribe()
+
+    if(this.scrollSubscription)
+      this.scrollSubscription.unsubscribe()
+  }
 }
